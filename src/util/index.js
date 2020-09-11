db71c2cfd19788a01c726a42937851e7749954ad
wangsongmao <wangsongmao@huobi.com>
2020-09-11 18:06:02 +0800

    chore

--- src/util/index.js
+++ src/util/index.js
@@ -1,14 +1,36 @@
 var fs = require("fs");
-var path = require("path");
-var { exec } = require("child_process");
+/*
+？遍历预估进度
+多进程异步处理
+读取进度报告，定时触发返回当前处理目录
+报错记录
+
+设置数据上限，和超时时间防止进入死循环
+
+
+
+数据更新机制
+  后台空闲遍历目录，可通过刷新按钮强制遍历
+
+记录文件读取缓存，缓存完整读取的目录，防止重复读取（可通过配置关闭）
+？后台运行目录读取
+
+
+
+
+*/
 function walkFilesAsync(path, cb) {
   var pa = fs.readdirSync(path);
   pa.forEach(function(ele, index) {
-    var info = fs.statSync(path + "/" + ele);
-    if (info.isDirectory()) {
-      walkFilesAsync(path + "/" + ele);
-    } else {
-      cb && cb(ele);
+    try {
+      var info = fs.statSync(path + "/" + ele);
+      if (info.isDirectory()) {
+        walkFilesAsync(path + "/" + ele);
+      } else {
+        cb && cb(ele);
+      }
+    } catch (ex) {
+      console.log(ex);
     }
   });
 }
