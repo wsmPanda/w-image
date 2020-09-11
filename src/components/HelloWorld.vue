678590e73122d73d13dd495f0b5d9d7be3b7f677
wangsongmao <wangsongmao@huobi.com>
2020-09-11 15:22:12 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <img alt="Vue logo" src="file:///Users/wangsongmao/Downloads/1%20(4).png" />
+    <button @click="onClick">select dir</button>
+  </div>
+</template>
+
+<script>
+//import { remote } from "electron";
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  methods: {
+    async onClick() {}
+  }
+};
+</script>
---
+++ src/util/index.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var path = require("path");
+var { exec } = require("child_process");
+function walkFilesAsync(path, cb) {
+  var pa = fs.walkFiles(path);
+  pa.forEach(function(ele, index) {
+    var info = fs.statSync(path + "/" + ele);
+    if (info.isDirectory()) {
+      walkFilesAsync(path + "/" + ele);
+    } else {
+      cb && cb(ele);
+    }
+  });
+}
+
+export { walkFilesAsync };
