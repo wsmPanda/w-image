c1e1c7a3c28a5e5e7a10469d8ecf641d72778676
wangsongmao <wangsongmao@huobi.com>
2021-08-12 16:14:07 +0800

    t1

--- tools/copy-webviewer-files.js
+++ tools/copy-webviewer-files.js
@@ -1,10 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
-
-<<<<<<< HEAD
-
-=======
->>>>>>> e26f611... feat: 同步代码
+// t1
 const copyFiles = async () => {
   const sourcePath = path.resolve('./node_modules/@pdftron/webviewer/public');
   const destPath = path.resolve('./public/webviewer/');
